e2dc8b1e76ab180f838507d62122925e6a91232b
Jun Lee <jy45lee@gmail.com>
2019-04-10 20:54:19 -0700

    starting local storage

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var pixels,waitTime,timeout;
+var pixels,waitTime,timeout, sliderVal;
 var startScroll = document.getElementById('startScroll');
 var stopScroll  = document.getElementById('stopScroll');
 var slider      = document.getElementById("myRange");
@@ -14,26 +14,45 @@ stopScroll.onclick = function(){
     pixels = 0;
 };
 
+document.onreadystatechange = function() {
+    console.log("local storage");
+    var saved_timeout;
+    var saved_pixel;
+    var saved_slider;
+
+    saved_timeout   = localStorage.getItem("timeout");
+    saved_pixel     = localStorage.getItem("pixel");
+    saved_slider    = localStorage.getItem("slider");
+
+    if(saved_timeout)   timeout     = saved_timeout;
+    if(saved_pixel)     pixels      = saved_pixel;
+    if(saved_slider)    sliderVal   = saved_slider;
+};
+
 function setSlider(){
     clearTimeout(timeout);
-
-    if(slider.value < 300)
+    if (slider.value < 300)
         waitTime = 300 - slider.value;
     else
         waitTime = 1;
+
     pixels = 1;
-    if(slider.value >= 300)
+    if (slider.value >= 300)
         pixels = 2;
-    else if(slider.value >= 350)
+    else if (slider.value >= 350)
         pixels = 3;
-    else if(slider.value >= 400)
+    else if (slider.value >= 400)
         pixels = 4;
-    else if(slider.value >= 450)
+    else if (slider.value >= 450)
         pixels = 5;
-    else if(slider.value >= 500)
+    else if (slider.value >= 500)
         pixels = 6;
 
     timeout = setInterval(scroll, waitTime);
+
+    localStorage.setItem("pixels",  JSON.stringify(pixels));
+    localStorage.setItem("timeout", JSON.stringify(timeout));
+    localStorage.setItem("slider",  JSON.stringify(slider.value));
 }
 
 function scroll(element) {
